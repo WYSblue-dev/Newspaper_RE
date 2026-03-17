6ca94a29ced9b1c26192ab82c554953f45cb7772
William D. Jerrells iii <Willjerrells@gmail.com>
2026-03-17 14:11:34 -0400

    added and tested CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -15,8 +15,31 @@ class CustomUserAdmin(UserAdmin):
         "email",
     ]
 
-    fieldsets = UserAdmin.fieldsets + ("age",)
-    add_fieldsets = UserAdmin.add_fieldsets + ("age",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {"fields": ("age",)},
+        ),
+    )
+    # due to the newer usable password of django we can't use the old feilds
+    # we must define ourselves. Due to django trying to say a password exist
+    # even though we're trying to create.
+    add_fieldsets = (
+        (
+            None,
+            {
+                # must add password fields here for us to enter for the account
+                # we create
+                "fields": (
+                    "username",
+                    "email",
+                    "age",
+                    "password1",
+                    "password2",
+                )
+            },
+        ),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
