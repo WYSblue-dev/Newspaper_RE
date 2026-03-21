91abbee3fc2126f9f0e788b042b8dd174cfe878e
William D. Jerrells iii <Willjerrells@gmail.com>
2026-03-21 16:23:08 -0400

    added in the admin interface for the new model

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Article
+
 
 # Register your models here.
+class ArticleAdmin(ModelAdmin):
+    model = Article
+    list_display = [
+        "title",
+        "author",
+        "date_added",
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
